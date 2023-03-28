804b32f80edcad2575c8a36d20b5cc763bb98d0f
Hugo Macedo <22848633+hhmacedo@users.noreply.github.com>
2023-03-28 11:28:35 -0300

    Check for missing design variables and add others years to design

--- R/pnad_design.R
+++ R/pnad_design.R
@@ -9,14 +9,29 @@ pnad_design <- function(df) {
   df_year <- unique(as.numeric(df$V0101))
   if (df_year < 100) { df_year <- 1900 + df_year }
 
+  design_vars <- dplyr::case_when(
+    df_year %in% 1992:1999 ~ list(c("psu", "strat", "V4610", "V4609")),
+    df_year == 2001 ~ list(c("PSU", "STRAT", "V4610", "V4609")),
+    df_year %in% 2004:2009 ~ list(c("V4618", "V4617", "V4619", "V4610", "V4609")),
+    df_year %in% 2002:2015 ~ list(c("V4618", "V4617", "V4610", "V4609"))
+  )
+  design_vars <- unlist(design_vars)
+
+  # Check if all design variables are ok and stop if not
+  if (!all(design_vars %in% colnames(df))) {
+    missing_vars <- design_vars[!(design_vars %in% colnames(df))]
+    stop(paste0("The package couldn't find the following design vars:\n  ",
+                paste(missing_vars, collapse = ", "), "."))
+  }
+
   design_vars <- data.frame(
-    year = c(1998:1999, 2001:2009, 2011:2015),
-    pre_id = c(rep("~PSU", 3), rep("~V4618", 13)),
-    pre_strata = c(rep("~STRAT", 3), rep("~V4617", 13)),
-    pre_weights = c(rep("~as.numeric(V4610)", 5),
-                    rep("~(as.numeric(V4619) * as.numeric(V4610))", 6),
-                    rep("~as.numeric(V4610)", 5)),
-    post_strata = rep("~pos_estrato", 16)
+    year = c(1992:1993, 1995:1999, 2001:2009, 2011:2015),
+    pre_id = c(rep("~psu", 7), "~PSU", rep("~V4618", 13)),
+    pre_strata = c(rep("~strat", 7), "~STRAT", rep("~V4617", 13)),
+    pre_weights = c(rep("~as.numeric(V4610)", 10),
+                   rep("~(as.numeric(V4619) * as.numeric(V4610))", 6),
+                   rep("~as.numeric(V4610)", 5)),
+    post_strata = rep("~V4609", 21)
   )
 
   design_vars <- design_vars[design_vars$year == df_year, ]
